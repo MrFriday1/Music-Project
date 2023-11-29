dd59601e666b82ae318c80746fa9138c1fc74a48
MrFriday <71829924+MrFriday1@users.noreply.github.com>
2023-11-29 09:39:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector("html").addEventListener("click", start);
+document.querySelector("html").addEventListener("keypress", end);
+
+var startTime, endTime;
+var div = document.querySelector(".print")
+
+function start() {
+    startTime = new Date();
+};
+
+function end() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime;
+    var li = document.createElement("li")
+    var textNode = document.createTextNode(timeDiff);
+    li.appendChild(textNode);
+
+    div.appendChild(li);
+}
